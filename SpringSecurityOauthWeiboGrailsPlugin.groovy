35fc2a11c1c79994d3f3978751e7bef2748241f2
BeaVe <donbeave@gmail.com>
2013-11-25 16:11:43 +0800

    Initial commit.

---
+++ SpringSecurityOauthWeiboGrailsPlugin.groovy
@@ -0,0 +1,26 @@
+/**
+* @author <a href='mailto:donbeave@gmail.com'>Alexey Zhokhov</a>
+*/
+class SpringSecurityOauthWeiboGrailsPlugin {
+    def version = "0.1"
+    def grailsVersion = "1.2.2 > *"
+    def dependsOn = [springSecurityOauth: '2.0.2 > *']
+    def pluginExcludes = [
+        "grails-app/views/error.gsp"
+    ]
+
+    def title = "Weibo for Spring Security OAuth plugin"
+    def author = "Mihai Cazacu, Enrico Comiti, Alexey Zhokhov"
+    def authorEmail = "donbeave@gmail.com"
+    def description = '''\
+Integrate [Weibo|http://www.weibo.com] to [Spring Security OAuth plugin|http://grails.org/plugin/spring-security-oauth].
+'''
+
+    def documentation = "https://github.com/donbeave/grails-spring-security-oauth-weibo"
+
+    def license = "APACHE"
+
+    def developers = [[name: "Alexey Zhokhov", email: "donbeave@gmail.com"]]
+
+    def scm = [url: 'https://github.com/donbeave/grails-spring-security-oauth-weibo']
+}
---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,29 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+
+grails.project.dependency.resolver = "maven"
+grails.project.dependency.resolution = {
+    inherits("global") {
+
+    }
+    log "warn"
+    repositories {
+        grailsCentral()
+        mavenLocal()
+        mavenCentral()
+
+        mavenRepo 'http://repo.spring.io/milestone'
+    }
+    dependencies {
+    }
+
+    plugins {
+        compile ':spring-security-oauth:2.0.2'
+
+        build(":release:3.0.1",
+              ":rest-client-builder:1.0.3") {
+            export = false
+        }
+    }
+}
